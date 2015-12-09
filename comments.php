314ee8fa915a5893123406f35be36f7c62030465
timothyhartanto <hartantotimothy@gmail.com>
2015-12-09 22:34:24 +0700

    Create comments.php

---
+++ comments.php
@@ -0,0 +1,54 @@
+<?php
+
+require("config.inc.php");
+
+//initial query
+$query = "Select * FROM comments";
+
+$query_params = array(
+        
+    );
+
+//execute query
+try {
+    $stmt   = $db->prepare($query);
+    $result = $stmt->execute($query_params);
+}
+catch (PDOException $ex) {
+    $response["success"] = 0;
+    $response["message"] = "Database Error!";
+    die(json_encode($response));
+}
+
+// Finally, we can retrieve all of the found rows into an array using fetchAll 
+$rows = $stmt->fetchAll();
+
+
+if ($rows) {
+    $response["success"] = 1;
+    $response["message"] = "Post Available!";
+    $response["posts"]   = array();
+    
+    foreach ($rows as $row) {
+        $post             = array();
+		$post["post_id"]  = $row["post_id"];
+        $post["username"] = $row["username"];
+        $post["title"]    = $row["title"];
+        $post["message"]  = $row["message"];
+        
+        
+        //update our repsonse JSON data
+        array_push($response["posts"], $post);
+    }
+    
+    // echoing JSON response
+    echo json_encode($response);
+    
+    
+} else {
+    $response["success"] = 0;
+    $response["message"] = "No Post Available!";
+    die(json_encode($response));
+}
+
+?>
